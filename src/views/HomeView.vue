d3d95c4ca8f3a59ab39f1bd36709870a389c2a1d
Antoinoe <noe.guillaumin@edu.devinci.fr>
2024-01-13 18:54:43 +0100

    Attempt to fix img at home 2

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -19,9 +19,9 @@ This portfolio is currently under construction, with several pieces of content y
 
       <div class="HomeNav">
         <HomeNavItem name="Projects" imageSrc ="./img/dm_capture_croped.png" />
-        <HomeNavItem name="Experiences" imageSrc ="../../public/img/experiences-croped.png"/>
-        <HomeNavItem name="Education" imageSrc ="../../public/img/education-croped.png"/>
-        <HomeNavItem name="Contact" imageSrc ="../../public/img/contact-cat-croped.png"/>
+        <HomeNavItem name="Experiences" imageSrc ="./img/experiences-croped.png"/>
+        <HomeNavItem name="Education" imageSrc ="./img/education-croped.png"/>
+        <HomeNavItem name="Contact" imageSrc ="./img/contact-cat-croped.png"/>
       </div>
     </div>
   </div>
